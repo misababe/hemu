2ce7e6a0d57b2755e07a535a03c40b4531b56eb8
misababe <misababe0222@163.com>
2016-05-16 11:28:35 +0800

    web

---
+++ js/demo_ajax_json.js
@@ -0,0 +1,11 @@
+{
+  "products": [
+  { "nam":"精纯香薰洗发水" , "src":"img/product/pro1.jpg" , "price":"88.5"},
+  { "nam":"精纯香薰护发精油" , "src":"img/product/pro2.jpg" , "price":"106"},
+  { "nam":"Loretta洗发水", "src":"img/product/pro3.jpg", "price":"236"},
+  { "nam":"爱茉莉护发精油", "src":"img/product/pro4.jpg", "price":"29.8"},
+  { "nam":"玫丽盼沙龙护发精油", "src":"img/product/pro5.jpg", "price":"336"},
+  { "nam":"Loretta护发套组", "src":"img/product/pro6.jpg", "price":"466"},
+  {"nam":"摩洛哥护发精油", "src":"img/product/pro7.jpg", "price":"322"}
+]
+}
